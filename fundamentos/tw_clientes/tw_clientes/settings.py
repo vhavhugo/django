229f27f00affabc6a80a698205572a51c7faf487
vhavhugo <vhavhugo@hotmail.com>
2022-12-10 23:48:16 -0300

    BD MYSQL

--- fundamentos/tw_clientes/tw_clientes/settings.py
+++ fundamentos/tw_clientes/tw_clientes/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'tw_clientes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tw_django_fundamentos',
+        'USER': 'root',
+        'PASSWORD': 'vhav',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
