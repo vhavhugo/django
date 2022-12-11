4e853f8a1d0e2b55639c565675a3e97618643eae
vhavhugo <vhavhugo@hotmail.com>
2022-12-11 01:38:04 -0300

    View

--- fundamentos/tw_clientes/clientes/views.py
+++ fundamentos/tw_clientes/clientes/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Cliente
 # Create your views here.
+
+def listar_clientes(request):
+    clientes = clientes.objects.all()
+    return render(request, 'clientes/lista_clientes.html', {'clientes':clientes})
